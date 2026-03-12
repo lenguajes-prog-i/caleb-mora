d24e87faea14c1437e8f5d91272e70a22b79d066
CalebMora39 <camora8930@gmail.com>
2026-03-12 18:11:14 -0500

    corregimiento de algoritmo

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
-print("Caleb Mora y David Bedoya")
-print("Caleb Mora y David Bedoya")
-print("Caleb Mora y David Bedoya")
-print("Caleb Mora y David Bedoya")
-
+#print("Caleb Mora y David Bedoya")
+#print("Caleb Mora y David Bedoya")
+#print("Caleb Mora y David Bedoya")
+#print("Caleb Mora y David Bedoya")
+#-----------------------------------------------------------------------------------
+print("Ingrese el nombre")
 name = input()
 print(name)
 
 user = "admin@uniremington.com"
 pasword = "admin123"
+print("Ingrese el correo")
 user_input = input()
+print("Ingrese la clave")
 pasword_input = input()
 
 if user_input== user and pasword_input== pasword:
     print("bienvenido al sistema")
 else:
     print("usuarion no encontrado")
+    
